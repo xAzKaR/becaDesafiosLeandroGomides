fff6e12c1bf0f9f1c6041689807b7c46a8689c7f
Leandro Gomides <leandro.limacarneiro@nttdata.com>
2022-01-20 13:46:57 -0300

    Adicionando projeto na pasta develop

---
+++ src/main/java/io/github/leandrogomides/nttMED/Consulta.java
@@ -0,0 +1,10 @@
+package nttMED;
+
+public class Consulta {
+
+    final Medico medico;
+
+    public Consulta(Medico medico) {
+        this.medico = medico;
+    }
+}
---
+++ src/test/java/io/github/leandrogomides/nttMED/NttMedApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leandrogomides.nttMED;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
