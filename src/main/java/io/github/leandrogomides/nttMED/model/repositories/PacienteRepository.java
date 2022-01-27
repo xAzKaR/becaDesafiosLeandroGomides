84342dc5d01c75e2812ea162f04a50b2b42609d0
Leandro Gomides <leandro.limacarneiro@nttdata.com>
2022-01-27 19:16:51 -0300

    Removendo arquivos que não fazem parte do desafio 33

--- src/main/java/io/github/leandrogomides/nttMED/model/repositories/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-//package io.github.leandrogomides.nttMED.model.repositories;
-//
-//import io.github.leandrogomides.nttMED.model.entities.Paciente;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-//}
